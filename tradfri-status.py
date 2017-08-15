1ea6b6aff6e8c93a8bd6962db661e10967a99802
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2017-08-15 18:41:05 +0200

    allow configuration via environment

--- tradfri-status.py
+++ tradfri-status.py
@@ -25,6 +25,7 @@
 
 from __future__ import print_function
 
+import os
 import sys
 import time
 import ConfigParser
@@ -34,11 +35,17 @@ from tqdm import tqdm
 
 def main():
     """ main function """
-    conf = ConfigParser.ConfigParser()
-    conf.read('tradfri.cfg')
+    hubip = os.environ.get("GATEWAY_IP", None)
+    securityid = os.environ.get("GATEWAY_KEY", None)
 
-    hubip = conf.get('tradfri', 'hubip')
-    securityid = conf.get('tradfri', 'securityid')
+    if not hubip:
+        conf = ConfigParser.ConfigParser()
+        conf.read('tradfri.cfg')
+        hubip = conf.get('tradfri', 'hubip')
+    if not securityid:
+        conf = ConfigParser.ConfigParser()
+        conf.read('tradfri.cfg')
+        securityid = conf.get('tradfri', 'securityid')
 
     lightbulb = []
     lightgroup = []
